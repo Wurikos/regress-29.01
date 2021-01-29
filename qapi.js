2393831f68e2e98fc7e6c92597cc7832bb16d8ee
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-01-29 11:17:29 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,52 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6013c2fef9d679001eab01fc",
+				"6013c2fef9d679001eab01fe",
+				"6013c309f9d679001eab0206",
+				"6013c30d9af38b0022e530f0"
+			]
+		},
+		"6013c2fef9d679001eab01fc": {
+			"id": "6013c2fef9d679001eab01fc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6013c2fef9d679001eab01fe": {
+			"id": "6013c2fef9d679001eab01fe",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"6013c309f9d679001eab0206": {
+			"id": "6013c309f9d679001eab0206",
+			"pageUrl": "contact",
+			"name": "contact"
+		},
+		"6013c30d9af38b0022e530f0": {
+			"id": "6013c30d9af38b0022e530f0",
+			"pageUrl": "about",
+			"name": "about",
+			"customCode": {
+				"6013c3e6e030c0e2a9d89629": {
+					"name": "script",
+					"place": "endOfHead",
+					"tag": "script",
+					"data": {
+						"content": "console.log(\"testpageABOUT\")",
+						"async": true
+					}
+				}
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,108 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Box, Button, Hr, Icon, Image, Input, Text, List } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Menu, Formspree, GoogleMap, Section, SocialMedia, StackItem, Stack } from "@quarkly/components";
+import { MdFace } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Box />
+		<Button>
+			Button
+		</Button>
+		<Menu />
+		<Hr />
+		<Icon category="md" icon={MdFace} />
+		<Image width="64px" height="64px" />
+		<Input />
+		<Link href="#">
+			Some text
+		</Link>
+		<List>
+			<Text>
+				First item
+			</Text>
+		</List>
+		<Text>
+			Some text
+		</Text>
+		<Formspree errorMessage="Something went wrong" completeText="Success">
+			<Input name="email" placeholder="Email" margin-right="4px" />
+			<Button>
+				Submit
+			</Button>
+		</Formspree>
+		<GoogleMap />
+		<Menu />
+		<Section />
+		<SocialMedia
+			facebook="https://www.facebook.com/quarklyapp/"
+			twitter="https://twitter.com/quarklyapp"
+			youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+			vkontakte="https://vk.com/quarklyapp"
+			telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+		/>
+		<Stack>
+			{"    "}
+			<StackItem width="50%" display="flex">
+				{"        "}
+				<Text font="--lead" margin="0px 0px 0px 0px" display="inline-block">
+					{"            "}First Item{"\n        "}
+				</Text>
+				{"    "}
+			</StackItem>
+			{"    "}
+			<StackItem width="50%" display="flex">
+				{"        "}
+				<Text font="--lead" margin="0px 0px 0px 0px" display="inline-block">
+					{"            "}Second Item{"\n        "}
+				</Text>
+				{"    "}
+			</StackItem>
+		</Stack>
+		<StackItem>
+			<Text font="--lead">
+				Stack item
+			</Text>
+		</StackItem>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<script async={true} place={"endOfHead"} rawKey={"6013c3c775c10bc40a2aae0a"}>
+				{"console.log (\"ТЕСТ\")"}
+			</script>
+			<style place={"endOfHead"} rawKey={"6013c40bd358218033b3287f"}>
+				{"body {\nbackground-color:red}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
